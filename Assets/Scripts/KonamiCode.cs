5313e6f074486506f609a808784ee0d3c3410fdf
Dan <112936289+DanZo0x@users.noreply.github.com>
2025-09-03 14:40:41 +0200

    [ADD] Konami code + scenes + score placeholder + transition

---
+++ Assets/Scripts/KonamiCode.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class KonamiCode : MonoBehaviour
+{
+    // The Konami code sequence
+    private KeyCode[] konamiCode = new KeyCode[]
+    {
+        KeyCode.UpArrow, KeyCode.UpArrow, KeyCode.DownArrow, KeyCode.DownArrow, KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.B, KeyCode.Q
+    };
+
+    private int currentIndex = 0;
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (Input.GetKeyDown(konamiCode[currentIndex]))
+            {
+                currentIndex++;
+                Debug.Log("Correct");
+
+                if (currentIndex >= konamiCode.Length)
+                {
+                    Debug.Log("Konami code activated!");
+                    DNDOL.Instance.bKonami = true;
+                    currentIndex = 0;
+                }
+            }
+            else
+            {
+                currentIndex = 0;
+                Debug.Log("Incorrect");
+            }
+        }
+    }
+}
