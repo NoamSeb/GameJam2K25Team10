07ea220724e5b4f340cd0aaf1506c504cd0b15ac
Dan <112936289+DanZo0x@users.noreply.github.com>
2025-09-04 15:35:01 +0200

    [ADD] Items fall

--- Assets/Scripts/Ropes/RopeBase.cs
+++ Assets/Scripts/Ropes/RopeBase.cs
@@ -1,10 +1,16 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class RopeBase : MonoBehaviour
 {
     [SerializeField] private bool bHasBeenLit;
     [SerializeField] private bool bIsBeingLit;
     [SerializeField] private float life;
+
+    [FormerlySerializedAs("fallMultiplier")] [Range(0, 10)] [SerializeField] private int fallTime;
+    [Range(-100, 0)] [SerializeField] private float fallDistance;
     
     public bool BHasBeenLit
     {
@@ -22,5 +28,24 @@ public class RopeBase : MonoBehaviour
     {
         get => life;
         set => life = value;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Fall());
+    }
+
+    private IEnumerator Fall()
+    {
+        float timesSinceStart = 0.0f;
+        Vector2 targetPosition = new Vector2(transform.position.x, transform.position.y + fallDistance);
+
+        while (timesSinceStart <= fallTime)
+        {
+            yield return null;
+            
+            transform.position = Vector2.Lerp(transform.position, targetPosition, timesSinceStart / fallTime);
+            timesSinceStart += Time.deltaTime;
+        }
     }
 }
