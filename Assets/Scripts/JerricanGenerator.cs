2c08ce6c71e8907ac28f5bfa0bfbe75ae46ae286
Dan <112936289+DanZo0x@users.noreply.github.com>
2025-09-04 14:30:22 +0200

    [ADD] Jerrycan generator

---
+++ Assets/Scripts/JerricanGenerator.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class JerricanGenerator : MonoBehaviour
+{
+    [SerializeField] private bool bHasJerrycan;
+    [Range(0, 100)] [SerializeField] private int jerrycanSpawnChance;
+    
+    [Header("Ropes Sprites")]
+    [SerializeField] private Sprite jerrycanSprite;
+    
+    void Start()
+    {
+        int ropeTypeIndex = Random.Range(0, 100);
+        bHasJerrycan = ropeTypeIndex >= jerrycanSpawnChance;
+
+        if (!bHasJerrycan)
+        {
+            return;
+        }
+
+        GetComponent<SpriteRenderer>().sprite = jerrycanSprite;
+    }
+}
